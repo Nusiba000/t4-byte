8608e5e11c1cbedca13cab6469078db2e571555e
Mahmood <msssals54321@gmail.com>
2025-05-18 12:35:21 +0400

    mod

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 num_1 = int(input("Enter the first number:"))
 num_2 = int(input("Enter the second number:"))
-opreation = input("Enter one of + - * ** / // and % : ")
+opreation = input("Enter one of + - * ** / // mod and % : ")
 
 if opreation == "+":
      result = num_1 + num_2
@@ -16,6 +16,7 @@ elif opreation=="//":
      result = num_1 // num_2
 elif opreation=="mod":
      result = num_1 % num_2
+
 else:
    print("opreation not supported")
 
