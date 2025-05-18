4b2016c11e10053f8f3f133d68746c2b7cfe657b
Nusiba <nusibaalnabhani@gmail.com>
2025-05-18 11:47:52 +0400

    test

--- calculator.py
+++ calculator.py
@@ -8,6 +8,8 @@ elif opreation=="-":
      result = num_1 - num_2
 elif opreation=="*":
      result = num_1 * num_2
+elif opreation=="*":
+     result = num_1 * num_2
 
 else:
    print("opreation not supported")
