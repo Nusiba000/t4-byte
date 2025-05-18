48c6b8b28e1025ff428481ca56adc65ac1010d5e
Nusiba <nusibaalnabhani@gmail.com>
2025-05-18 11:25:40 +0400

    test

---
+++ calculator.py
@@ -0,0 +1,12 @@
+num_1 = int(input("Enter the first number:"))
+num_2 = int(input("Enter the second number:"))
+opreation = input("Enter one of + : ")
+
+if opreation == "+":
+     result = num_1 + num_2
+
+else:
+   print("opreation not supported")
+
+print("Result is: ", result)
+print("End of Script")
